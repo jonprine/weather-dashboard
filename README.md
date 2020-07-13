48ee7f3dca9b873f5f87cd0e6b2b6326866e7c31
Vox Humana <jonprine@gmail.com>
2020-07-12 21:21:38 -0500

    fixed margin on 5day

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Purpose
+To check the weather in cities to include temperature, humidity, wind speed, uv index, and a icon displaying current weather.  Also displays weather forecast for the next 5 days.  Search history to include past searched cities.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+* API
+
+## Website
+https://jonprine.github.io/weather-dashboard/
+
+## Contribution
+Made by Jon Prine
+
+## Screenshots of Finished Page
+![Weather](assets/images/screenshot1.png)
+![Weather](assets/images/screenshot2.png)
+![Weather](assets/images/screenshot3.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,6 @@ function citySearch() {
             console.log(data);
             cityInfo(data);
         });
-
     cityHistory();
     fiveDay();
 
@@ -106,51 +105,63 @@ function fiveDay() {
 function fiveDayForecast(forecast) {
     console.log(forecast);
 
-    for (var i = 0; i <= 5; i++) {
+    for (var i = 0; i < 5; i++) {
 
+        // 5 Day Forecast
         var five = document.getElementById("five");
         five.textContent = "5 Day Forecast:";
+
         var date2 = document.getElementById("day-2");
         date2.textContent = day2;
+        var iconHTML2 = document.getElementById("day-2-icon");
+        var icon2 = forecast.list[0].weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/wn/" + icon2 + "@2x.png";
+        iconHTML2.setAttribute("src", weatherIcon);
         var temp2 = document.getElementById("temp-2")
         temp2.textContent = "Temp: " + (Math.round(((forecast.list[0].main.temp * 9 / 5) - 459.67))) + " " + "°F";
         var humid2 = document.getElementById("humid-2");
         humid2.textContent = "Humidity:" + " " + forecast.list[0].main.humidity + "%";
 
 
-
-
-
         var date3 = document.getElementById("day-3");
         date3.textContent = day3;
+        var iconHTML3 = document.getElementById("day-3-icon");
+        var icon3 = forecast.list[1].weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/wn/" + icon3 + "@2x.png";
+        iconHTML3.setAttribute("src", weatherIcon);
         var temp3 = document.getElementById("temp-3")
         temp3.textContent = "Temp: " + (Math.round(((forecast.list[1].main.temp * 9 / 5) - 459.67))) + " " + "°F";
         var humid3 = document.getElementById("humid-3");
         humid3.textContent = "Humidity:" + " " + forecast.list[1].main.humidity + "%";
 
-
-
         var date4 = document.getElementById("day-4");
         date4.textContent = day4;
+        var iconHTML4 = document.getElementById("day-4-icon");
+        var icon4 = forecast.list[2].weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/wn/" + icon4 + "@2x.png";
+        iconHTML4.setAttribute("src", weatherIcon);
         var temp4 = document.getElementById("temp-4")
         temp4.textContent = "Temp: " + (Math.round(((forecast.list[2].main.temp * 9 / 5) - 459.67))) + " " + "°F";
         var humid4 = document.getElementById("humid-4");
         humid4.textContent = "Humidity:" + " " + forecast.list[2].main.humidity + "%";
 
-
-
-
         var date5 = document.getElementById("day-5");
         date5.textContent = day5;
+        var iconHTML5 = document.getElementById("day-5-icon");
+        var icon5 = forecast.list[3].weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/wn/" + icon5 + "@2x.png";
+        iconHTML5.setAttribute("src", weatherIcon);
         var temp5 = document.getElementById("temp-5")
         temp5.textContent = "Temp: " + (Math.round(((forecast.list[2].main.temp * 9 / 5) - 459.67))) + " " + "°F";
         var humid5 = document.getElementById("humid-5");
         humid5.textContent = "Humidity:" + " " + forecast.list[1].main.humidity + "%";
 
-
-
         var date6 = document.getElementById("day-6");
         date6.textContent = day6;
+        var iconHTML6 = document.getElementById("day-6-icon");
+        var icon6 = forecast.list[4].weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/wn/" + icon6 + "@2x.png";
+        iconHTML6.setAttribute("src", weatherIcon);
         var temp6 = document.getElementById("temp-6")
         temp6.textContent = "Temp: " + (Math.round(((forecast.list[2].main.temp * 9 / 5) - 459.67))) + " " + "°F";
         var humid6 = document.getElementById("humid-6");
